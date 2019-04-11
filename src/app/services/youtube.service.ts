16f25e09ae093a44c8c5971c440b824596bb425b
Norberto Caceres <norbe1994@yahoo.com>
2019-04-11 15:54:18 -0500

    agregar metodo para get request para recibit playListItems

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpParams } from '@angular/common/http'
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http'
 import { map } from 'rxjs/operators'
 
 @Injectable({
@@ -7,19 +7,23 @@ import { map } from 'rxjs/operators'
 })
 export class YoutubeService {
   private youtubeUrl: string = 'https://www.googleapis.com/youtube/v3'
-  private apiKey: string = 'AIzaSyB7KswVhXCqIzsecWimQoknFFhQ5oht43k'
-  private playlist: string = 'UU8butISFwT-Wl7EV0hUK0BQ'
+  private apiKey: string = 'AIzaSyCaQwP-eWawlqfdIJ3fTsIXrhFkOZGvIjM'
+  private playlist: string = 'PL0vfts4VzfNiMQXc5zxl8--KDjaQcWNi2'
 
   constructor(public http: HttpClient) {}
 
   getVideos() {
-    let url = `${this.youtubeUrl}/playlistItems`
+    const url = `${this.youtubeUrl}/playlistItems`
     let params = new HttpParams()
-    params.set('part', 'snippet')
-    params.set('maxResults', '10')
-    params.set('playlistId', this.playlist)
-    params.set('apiKey', this.apiKey)
+    params = params.set('part', 'snippet')
+    params = params.set('maxresults', '10')
+    params = params.set('playlistId', this.playlist)
+    params = params.set('key', this.apiKey)
 
-    return this.http.get(url, { params }).pipe(map((res: any) => console.log(res)))
+    return this.http.get(url, { params }).pipe(
+      map((res: any) => {
+        console.log(res)
+      })
+    )
   }
 }
